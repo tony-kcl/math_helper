d5e292c562c3aea956ddf93f9fd81b7a2402d55f
TonyK <justdo.85@gmail.com>
2023-04-29 14:36:58 +0800

    First commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/kuoto/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kuoto/Projects/math_helper"
+export "FLUTTER_ROOT=/Users/tony/Library/Application Support/fvm/versions/3.3.9"
+export "FLUTTER_APPLICATION_PATH=/Users/tony/flutter_side_project/math_helper"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
